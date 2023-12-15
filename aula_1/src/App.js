d6e54ce535b64814af80fdae36bff9e66185e0c9
ViniMSLima <vinisary.lima@gmail.com>
2023-12-15 09:23:27 -0300

    cr7 componente

--- aula_1/src/App.js
+++ aula_1/src/App.js
@@ -1,29 +1,14 @@
 import logo from './logo.svg';
+import CR7 from "./components/FirstComponent";
 import './App.css';
 
 function App() {
   const teste = ["C","R","7"];
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={"https://parspng.com/wp-content/uploads/2022/10/Cristiano-Ronaldopng.parspng.com-8.png"} className="App-logo" alt="logo" />
-        <p>
-          SIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
-        </p>
-        <p>
-          {teste.map(t => t)}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <CR7></CR7>
+    </>
   );
 }
 
---
+++ aula_1/src/components/FirstComponent/index.jsx
@@ -0,0 +1,22 @@
+import style from "./styles.css"
+export default function CR7(){
+    return(
+        <div className="App">
+      <header className="App-header">
+        <img src={"https://parspng.com/wp-content/uploads/2022/10/Cristiano-Ronaldopng.parspng.com-8.png"} className="App-logo" alt="logo" />
+        <p>
+          SIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
+        </p>
+
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+    )
+};
