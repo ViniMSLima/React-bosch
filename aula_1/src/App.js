6d85bda51d81ef9fa4ddb16e0f1cf34261f28a57
ViniMSLima <vinisary.lima@gmail.com>
2023-12-15 11:56:05 -0300

    contador por tempo

--- aula_1/src/App.js
+++ aula_1/src/App.js
@@ -1,17 +1,39 @@
-// import logo from './logo.svg';
-// import CR7 from "./components/FirstComponent";
-// import Counter from "./components/Counter";
-// import Input from "./components/Input";
+import React, { useState, useEffect } from 'react';
+import style from './App.css';
 
-import Calculator from "./components/Calculator";
-import './App.css';
+const App = () => {
+ const [count, setCount] = useState(0);
+ const [pageTitle, setPageTitle] = useState('Initial Page Title');
+ const [notifVisible, setNotifVisible] = useState(false);
 
-function App() {
-  return (
-    <>
-      <Calculator/>
-    </>
-  );
-}
+ useEffect(() => {
+    document.title = pageTitle;
+    setNotifVisible(true);
+    setTimeout(() => {
+      setNotifVisible(false);
+    }, 1000);
+ }, [pageTitle]);
+
+ useEffect(() => {
+    const timer = setInterval(() => {
+      setCount((prevCount) => prevCount + 1);
+    }, 5000);
+    return () => clearInterval(timer);
+ }, []);
+
+ useEffect(() => {
+    setPageTitle(`Count: ${count}`);
+ }, [count]);
+
+ return (
+    <div>
+      <h1>Website Title: {pageTitle}</h1>
+      <div>
+
+      {notifVisible && <div style={{borderRadius: "20px", backgroundColor: "lightgreen", width: "200px", height: "100px", fontSize: "30px", fontWeight: "bold"}}>Title has been changed!</div>}
+      </div>
+    </div>
+ );
+};
 
 export default App;
